5120c8a427a84c8b389ba35b1e3661fe1067be1f
jinjieliu <jinjie.liu@usc.edu>
2025-02-21 03:14:42 -0800

    add -f in recompile script
    Signed-off-by: jinjieliu <jinjie.liu@usc.edu>

--- src/gfs/recompile.sh
+++ src/gfs/recompile.sh
@@ -1,5 +1,5 @@
-rm *.pyc
+rm -f *.pyc
 rm -rf __pycache__
-rm gfs_pb2_grpc.py
-rm gfs_pb2.py
+rm -f gfs_pb2_grpc.py
+rm -f gfs_pb2.py
 python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. ./gfs.proto
