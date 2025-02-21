ab71a2a29bc5788e1588493cf6a01f60a994af8a
xth87706565 <david.hong20010113@gmail.com>
2025-02-20 17:43:13 -0800

    started implementing chunk servers

---
+++ src/chunk_server/chunk_servicer.py
@@ -0,0 +1,18 @@
+import os
+import sys
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Add it to sys.path
+sys.path.append(parent_dir)
+import grpc
+import gfs_pb2_grpc
+import gfs_pb2
+
+from common import Config as cfg
+from common import Status
+class ChunkServerToClientServicer(gfs_pb2_grpc.ChunkServerToClientServicer):
+    def __init__(self):
+        pass
+
+    def Create(self, request, context):
+        
--- src/common.py
+++ src/common.py
@@ -4,6 +4,7 @@ class Config(object):
     master_loc = "104.248.169.227:50051"
     chunkserver_locs = ["50052", "50053", "50054", "50055", "50056"]
     chunkserver_root = "root_chunkserver"
+    default_chunk_size = 10 #megabytes
 
 
 class Status(object):
