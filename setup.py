27fb3b42a87aebbcc1ab94fc95fa8d1071508a35
jinjieliu <jinjie.liu@usc.edu>
2025-02-21 03:37:06 -0800

    move compilation into build script
    Signed-off-by: jinjieliu <jinjie.liu@usc.edu>

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
+import os
 import setuptools
+import setuptools.command
+import setuptools.command.build_py
+
+
+class BuildProtoCommand(setuptools.command.build_py.build_py):
+    def run(self):
+        import grpc_tools.protoc
+        root_dir: str = os.path.abspath(os.path.dirname(__file__))
+        gfs_dir: str = os.path.join(root_dir, "src", "gfs")
+        proto_path: str = os.path.join(gfs_dir, "gfs.proto")
+        grpc_tools.protoc.main(
+            (
+                "",
+                f"-I{gfs_dir}",
+                f"--python_out={gfs_dir}",
+                f"--grpc_python_out={gfs_dir}",
+                f"{proto_path}",
+            )
+        )
+        super().run(self)
+
 
 setuptools.setup(
     name="Google-File-System-Lite-Pro",
@@ -9,10 +31,14 @@ setuptools.setup(
     ],
     packages=setuptools.find_packages("src"),
     package_dir={"": "src"},
-    install_requires=[
-        "grpcio",
+    setup_requires=[
         "grpcio-tools",
     ],
+    install_requires=[
+        "grpcio",
+        "protobuf",
+    ],
+    cmdclass={"build_py": BuildProtoCommand},
     entry_points={
         "console_scripts": [
             "gfs-master=gfs.master_server.master_main:serve",
