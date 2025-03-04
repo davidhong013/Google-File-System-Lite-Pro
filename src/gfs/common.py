647188003313bfa8eba0f62459757fb7a2420620
xth87706565 <david.hong20010113@gmail.com>
2025-03-04 12:03:13 -0800

    testing

--- src/gfs/common.py
+++ src/gfs/common.py
@@ -1,8 +1,8 @@
 class Config(object):
     # left to be changed
     chunk_size = 5
-    master_loc = "165.232.154.137:50051"
-    chunkserver_locs = ["104.248.169.227:50051", "104.248.169.227:50052"]
+    master_loc = "35.95.188.241:50051"
+    chunkserver_locs = ["35.95.188.241:50052", "35.95.188.241:50053"]
     chunkserver_root = "root_chunkserver"
     default_chunk_size = 2 * 1024 * 1024# 2megabytes
     message_options = [
