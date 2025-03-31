645b91071a6ac8d67e9e8d60cb36d32e74b86346
jinjieliu <jinjie.liu@usc.edu>
2025-03-31 02:02:55 -0700

    add manual configs
    Signed-off-by: jinjieliu <jinjie.liu@usc.edu>

--- src/gfs/common.py
+++ src/gfs/common.py
@@ -1,18 +1,50 @@
-class Config(object):
-    # left to be changed
-    chunk_size = 3
-    master_loc = "35.95.188.241:50051"
-    chunkserver_locs = [
-        "35.95.188.241:50052",
-        "35.95.188.241:50053",
-        "35.95.188.241:50054",
-    ]
-    chunkserver_root = "root_chunkserver"
-    default_chunk_size = 1 * 1024 * 1024  # 2megabytes
-    message_options = [
-        ("grpc.max_receive_message_length", 100 * 1024 * 1024),  # 100MB limit
-        ("grpc.max_send_message_length", 100 * 1024 * 1024),  # 100MB limit
-    ]
+from io import TextIOWrapper
+from typing import Dict, Any, List, Optional, Tuple
+
+import json
+import os
+
+config_path: Optional[str] = os.getenv("GFS_CONFIG_PATH", None)
+
+
+class ConfigImpl(object):
+    def __init__(
+        self,
+        chunk_size: int = 3,
+        master_loc: str = "localhost:50051",
+        chunkserver_locs: List[str] = [
+            "localhost:50052",
+            "localhost:50053",
+            "localhost:50054",
+        ],
+        chunkserver_root: str = "root_chunkserver",
+        default_chunk_size: int = 1 * 1024 * 1024,
+        message_options: List[Tuple[str, int]] = [
+            ("grpc.max_receive_message_length", 100 * 1024 * 1024),  # 100MB limit
+            ("grpc.max_send_message_length", 100 * 1024 * 1024),  # 100MB limit
+        ],
+        *args,
+        **kwargs
+    ) -> "ConfigImpl":
+        super(ConfigImpl, self).__init__(*args, **kwargs)
+        self.chunk_size: int = chunk_size
+        self.master_loc: str = master_loc
+        self.chunkserver_locs: List[str] = chunkserver_locs
+        self.chunkserver_root: str = chunkserver_root
+        self.default_chunk_size: int = default_chunk_size
+        self.message_options: List[Tuple[str, int]] = message_options
+
+    @classmethod
+    def load(f: TextIOWrapper) -> "ConfigImpl":
+        config_json: Dict[str, Any] = json.load(f)
+        return ConfigImpl(**config_json)
+
+
+if config_path:
+    with open(config_path, "r") as f:
+        Config: ConfigImpl = ConfigImpl.load(f)
+else:
+    Config: ConfigImpl = ConfigImpl()
 
 
 class Status(object):
